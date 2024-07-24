00071f0d573207f17e15a0ea360135c8cf4258cb
Max K <TheZnat7@yandex.ru>
2024-07-24 15:11:22 +0300

    initialization

---
+++ food-app/src/App.tsx
@@ -0,0 +1,11 @@
+import Button from "./components/Button/Button";
+
+function App() {
+  return (
+    <>
+      <Button>Вход</Button>
+    </>
+  );
+}
+
+export default App;
---
+++ food-app/src/components/Button/Button.tsx
@@ -0,0 +1,9 @@
+import styles from "./Button.module.css";
+import { ButtonProps } from "./Button.props";
+import cn from "classnames";
+
+const Button = ({ children, className,...props }: ButtonProps) => {
+  return <button className={cn( styles['button'], className)} {...props}>{children}</button>;
+};
+
+export default Button;
